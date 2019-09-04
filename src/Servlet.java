73310e3864de1e81b8a992f2d96c8d9f12666ec1
zdobrzynski@my.wctc.edu <zdobrzynski@my.wctc.edu>
2019-09-04 14:53:39 -0500

    in class

---
+++ src/Servlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet", urlPatterns = "/hello")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String message = request.getParameter("type");
+        String full = "<p>You said " + message + "</p>";
+        response.getWriter().print(message);
+    }
+}
